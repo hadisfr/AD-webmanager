81f252320f960d729e9d97771653a9890c63a7d2
H@di <hadi@hadisafari.ir>
2026-06-26 08:26:11 +0330

    refine settings

--- settings.py
+++ settings.py
@@ -31,6 +31,7 @@ def get_ldap_server(ldap_domain: str):
 
 
 class Settings:
+    SECRET_KEY = config("CSRF_SECRET_KEY")
     LDAP_DOMAIN = config("LDAP_DOMAIN")
     LDAP_SERVER = config("LDAP_SERVER", get_ldap_server(LDAP_DOMAIN))
     LDAP_DN = config("LDAP_DN", "DC=%s" % ",DC=".join(LDAP_DOMAIN.split(".")))
@@ -48,10 +49,7 @@ class Settings:
         "CN=NTDS Quotas", "CN=Program Data", "CN=System",
         "OU=Domain Controllers", "CN=Guest", "CN=krbtgt"
     ]
-    SEARCH_ATTRS = [('sAMAccountName', 'Username'), ('givenName', 'Name')]
-    USER_ATTRIBUTES = [
-        ["jpegPhoto", "Photo"],
-    ]
+    SEARCH_ATTRS = [('sAMAccountName', 'Username'), ('displayName', 'Display Name')]
     TREE_ATTRIBUTES = [
         ['mail', "Email"], ['objectClass', "Type"], ['active', "Status"]
     ]
